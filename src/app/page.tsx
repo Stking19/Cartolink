55f70ecab40338223e25eb0291211311f0229776
Stking19 <okolistephen43@gmail.com>
2025-09-22 18:04:50 +0100

    mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,16 @@ import Footer from "@/components/Footer";
 
 export default function Page() {
   return (
-    <div className="w-full h-max bg-background text-foreground">
+    <div>
+      <div className="md:hidden flex h-screen items-center justify-center bg-gray-900 text-white text-center p-6">
+        <p>This site is only available on desktop. Please visit from a larger screen</p>
+      </div>
+    <div className="hidden md:block w-full h-max bg-background text-foreground">
       <Navbar />
       <Hero />
       <GenerateSection />
       <Footer />
+    </div>
     </div>
   );
 }
